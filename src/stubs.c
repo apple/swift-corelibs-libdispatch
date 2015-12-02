8d8bb462e8be1e1c1b79f21f5b61863d009c4fde
David Grove <groved@us.ibm.com>
2015-12-02 01:40:23 +0000

    stub out missing functions and statics with dummy definitions

---
+++ src/stubs.c
@@ -0,0 +1,48 @@
+#include "pthread.h"
+
+// void _dispatch_source_type_timer() { }
+//void dispatch_source_create() { }
+//void dispatch_source_set_event_handler_f() { }
+//void dispatch_source_set_timer() { }
+//void dispatch_resume() { }
+//void dispatch_main() { }
+void _voucher_create_accounting_voucher() { }
+void pthread_workqueue_attr_destroy_np() { }
+void dispatch_timer__program_semaphore() { }
+void kqueue() { }
+void _dispatch_mach_msg_dispose() { }
+void dispatch_timer__wake_semaphore() { }
+void _dispatch_mach_probe() { }
+void voucher_copy() { }
+void _dispatch_block_create() { }
+void _dispatch_mach_invoke() { }
+void _dispatch_mach_msg_debug() { }
+void _dispatch_mach_dispose() { }
+void TRASHIT() { }
+void _dispatch_mach_msg_invoke() { }
+void pthread_workqueue_attr_setovercommit_np() { }
+void _dispatch_runloop_queue_probe() { }
+void _voucher_thread_cleanup() { }
+void _dispatch_continuation_free_to_heap() { }
+void _dispatch_runloop_queue_xref_dispose() { }
+void pthread_workqueue_attr_setqueuepriority_np() { }
+void dispatch_timer__configure_semaphore() { }
+void _voucher_init() { }
+void strlcpy() { }
+void _dispatch_runloop_queue_dispose() { }
+void kevent64() { }
+void mach_error_string() { }
+void pthread_workqueue_additem_np() { }
+void _dispatch_block_special_invoke() { }
+void _dispatch_continuation_alloc_from_heap() { }
+void dispatch_timer__fire_semaphore() { }
+void mach_vm_deallocate() { }
+void pthread_mach_thread_np() { }
+void pthread_workqueue_create_np() { }
+void mach_task_self() { }
+void pthread_workqueue_attr_init_np() { }
+void _voucher_atfork_child() { }
+void sysctlbyname() { }
+
+pthread_key_t dispatch_voucher_key;
+pthread_key_t dispatch_pthread_root_queue_observer_hooks_key;
