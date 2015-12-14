e948f91b88675285c39e9949a4948c0d1feafb37
Katashi Monosato <katashi.monosato@gmail.com>
2015-12-14 08:35:15 -0800

    sprinkle the source with HAVE_MACH

--- src/shims/linux_stubs.c
+++ src/shims/linux_stubs.c
@@ -24,7 +24,7 @@
  */
 
 #include <stdint.h>
-
+#include <syscall.h>
 #include <config/config.h>
 
 #include "pthread.h"
@@ -35,25 +35,11 @@
 #undef LINUX_PORT_ERROR
 #define LINUX_PORT_ERROR()  do { printf("LINUX_PORT_ERROR_CALLED %s:%d: %s\n",__FILE__,__LINE__,__FUNCTION__); abort(); } while (0)
 
-void _dispatch_mach_msg_dispose() { LINUX_PORT_ERROR();  }
-
-unsigned long _dispatch_mach_probe(dispatch_mach_t dm) {
-  LINUX_PORT_ERROR();
-}
-
 dispatch_block_t _dispatch_block_create(dispatch_block_flags_t flags,
 					voucher_t voucher, pthread_priority_t priority,
 					dispatch_block_t block) {
   LINUX_PORT_ERROR();
 }
-
-void _dispatch_mach_invoke() { LINUX_PORT_ERROR();  }
-
-size_t _dispatch_mach_msg_debug(dispatch_mach_msg_t dmsg, char* buf, size_t bufsiz) {
-  LINUX_PORT_ERROR();
-}
-void _dispatch_mach_dispose() { LINUX_PORT_ERROR();  }
-void _dispatch_mach_msg_invoke() { LINUX_PORT_ERROR();  }
 
 unsigned long _dispatch_runloop_queue_probe(dispatch_queue_t dq) {
   LINUX_PORT_ERROR();
@@ -61,18 +47,10 @@ unsigned long _dispatch_runloop_queue_probe(dispatch_queue_t dq) {
 void _dispatch_runloop_queue_xref_dispose() { LINUX_PORT_ERROR();  }
 
 void _dispatch_runloop_queue_dispose() { LINUX_PORT_ERROR();  }
-char* mach_error_string(mach_msg_return_t x) {
-  LINUX_PORT_ERROR();
-}
-
 void mach_vm_deallocate() { LINUX_PORT_ERROR();  }
 
 mach_port_t pthread_mach_thread_np() {
-  return (mach_port_t)pthread_self();
-}
-
-mach_port_t mach_task_self() {
-  return (mach_port_t)pthread_self();
+  return (pid_t)syscall(SYS_gettid);
 }
 
 /*
