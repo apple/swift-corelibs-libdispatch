3bbed1bce14d8bcbe0ac5f95f832a272bd99f7c8
Daniel A. Steffen <dsteffen@apple.com>
2016-07-13 17:14:51 -0700

    whitespace
    Signed-off-by: Daniel A. Steffen <dsteffen@apple.com>

--- src/swift/Queue.swift
+++ src/swift/Queue.swift
@@ -246,7 +246,7 @@ public extension DispatchQueue {
 	private func _syncHelper<T>(
 		fn: (@noescape () -> ()) -> (), 
 		execute work: @noescape () throws -> T, 
-		rescue: ((Swift.Error) throws -> (T))) rethrows -> T 
+		rescue: ((Swift.Error) throws -> (T))) rethrows -> T
 	{
 		var result: T?
 		var error: Swift.Error?
@@ -269,7 +269,7 @@ public extension DispatchQueue {
 		fn: (DispatchWorkItem) -> (), 
 		flags: DispatchWorkItemFlags,
 		execute work: @noescape () throws -> T,
-		rescue: ((Swift.Error) throws -> (T))) rethrows -> T 
+		rescue: ((Swift.Error) throws -> (T))) rethrows -> T
 	{
 		var result: T?
 		var error: Swift.Error?
