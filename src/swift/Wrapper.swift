2ab938a278c3452d0be44b4f5bff0dfc60bedaf5
Daniel A. Steffen <dsteffen@apple.com>
2016-07-13 17:14:07 -0700

    whitespace
    Signed-off-by: Daniel A. Steffen <dsteffen@apple.com>

--- src/swift/Wrapper.swift
+++ src/swift/Wrapper.swift
@@ -316,4 +316,4 @@ internal enum _OSQoSClass : UInt32  {
 }
 
 @_silgen_name("_swift_dispatch_release")
-internal func _swift_dispatch_release(_ obj: dispatch_object_t) -> Void 
+internal func _swift_dispatch_release(_ obj: dispatch_object_t) -> Void
