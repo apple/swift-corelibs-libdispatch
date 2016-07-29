8145a073908af8e41356c0dd94421e71c6d5e92c
Rintaro Ishizaki <fs.output@gmail.com>
2016-07-29 20:40:54 +0900

    [SE-0101] .size to .stride
    Signed-off-by: Daniel A. Steffen <dsteffen@apple.com>

--- src/swift/Data.swift
+++ src/swift/Data.swift
@@ -154,7 +154,7 @@ public struct DispatchData : RandomAccessCollection {
 	
 	/// Copy the contents of the data into a buffer.
 	///
-	/// This function copies the bytes in `range` from the data into the buffer. If the count of the `range` is greater than `MemoryLayout<DestinationType>.size * buffer.count` then the first N bytes will be copied into the buffer.
+	/// This function copies the bytes in `range` from the data into the buffer. If the count of the `range` is greater than `MemoryLayout<DestinationType>.stride * buffer.count` then the first N bytes will be copied into the buffer.
 	/// - precondition: The range must be within the bounds of the data. Otherwise `fatalError` is called.
 	/// - parameter buffer: A buffer to copy the data into.
 	/// - parameter range: A range in the data to copy into the buffer. If the range is empty, this function will return 0 without copying anything. If the range is nil, as much data as will fit into `buffer` is copied.
@@ -172,9 +172,9 @@ public struct DispatchData : RandomAccessCollection {
 			precondition(r.endIndex >= 0)
 			precondition(r.endIndex <= cnt, "The range is outside the bounds of the data")
 			
-			copyRange = r.startIndex..<(r.startIndex + Swift.min(buffer.count * MemoryLayout<DestinationType>.size, r.count))
+			copyRange = r.startIndex..<(r.startIndex + Swift.min(buffer.count * MemoryLayout<DestinationType>.stride, r.count))
 		} else {
-			copyRange = 0..<Swift.min(buffer.count * MemoryLayout<DestinationType>.size, cnt)
+			copyRange = 0..<Swift.min(buffer.count * MemoryLayout<DestinationType>.stride, cnt)
 		}
 		
 		guard !copyRange.isEmpty else { return 0 }
