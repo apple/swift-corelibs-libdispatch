dac881a0ec3e41b669aac1f2d686e90e665f08ae
Johannes Weiß <johannesweiss@apple.com>
2017-01-16 19:37:06 +0000

    don't hold references to unowned DispatchData objects (SR-3628)

--- src/swift/DispatchStubs.cc
+++ src/swift/DispatchStubs.cc
@@ -165,6 +165,12 @@ _swift_dispatch_release(dispatch_object_t obj) {
   dispatch_release(obj);
 }
 
+SWIFT_CC(swift) DISPATCH_RUNTIME_STDLIB_INTERFACE
+extern "C" void
+_swift_dispatch_retain(dispatch_object_t obj) {
+  dispatch_retain(obj);
+}
+
 // DISPATCH_RUNTIME_STDLIB_INTERFACE
 // extern "C" dispatch_queue_t
 // _swift_apply_current_root_queue() {
