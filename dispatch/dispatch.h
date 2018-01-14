apple/swift-corelibs-libdispatch

commit 8af68ef3e4bbc1ad94af27045950467086cf6305

fix invalid use of config defines in API/SPI headers
Signed-off-by: Daniel A. Steffen <dsteffen@apple.com>

dispatch/dispatch.h
@@ -24,30 +24,17 @@
 #ifdef __APPLE__
 #include <Availability.h>
 #include <TargetConditionals.h>
-#else
-#ifndef API_AVAILABLE
-#define API_AVAILABLE(...)
+#include <os/base.h>
+#elif defined(__linux__)
+#include <os/linux_base.h>
 #endif
-#ifndef API_DEPRECATED
-#define API_DEPRECATED(...)
-#endif
-#ifndef API_UNAVAILABLE
-#define API_UNAVAILABLE(...)
-#endif
-#ifndef API_DEPRECATED_WITH_REPLACEMENT
-#define API_DEPRECATED_WITH_REPLACEMENT(...)
-#endif
-#endif // __APPLE__
 
-#if HAVE_SYS_CDEFS_H
-#include <sys/cdefs.h>
-#endif
 #include <sys/types.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdarg.h>
-#if HAVE_UNISTD_H
+#if !defined(HAVE_UNISTD_H) || HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 #include <fcntl.h>

os/linux_base.h
@@ -15,6 +15,23 @@
 
 #include <sys/param.h>
 
+#if HAVE_SYS_CDEFS_H
+#include <sys/cdefs.h>
+#endif
+
+#ifndef API_AVAILABLE
+#define API_AVAILABLE(...)
+#endif
+#ifndef API_DEPRECATED
+#define API_DEPRECATED(...)
+#endif
+#ifndef API_UNAVAILABLE
+#define API_UNAVAILABLE(...)
+#endif
+#ifndef API_DEPRECATED_WITH_REPLACEMENT
+#define API_DEPRECATED_WITH_REPLACEMENT(...)
+#endif
+
 #if __GNUC__
 #define OS_EXPECT(x, v) __builtin_expect((x), (v))
 #define OS_UNUSED __attribute__((__unused__))

os/object.h
@@ -23,13 +23,11 @@
 
 #ifdef __APPLE__
 #include <Availability.h>
-#include <TargetConditionals.h>
 #include <os/availability.h>
-#endif
-#ifdef __linux__
-#include <os/linux_base.h>
-#else
+#include <TargetConditionals.h>
 #include <os/base.h>
+#elif defined(__linux__)
+#include <os/linux_base.h>
 #endif
 
 /*!

os/object_private.h
@@ -27,15 +27,9 @@
 #ifndef __OS_OBJECT_PRIVATE__
 #define __OS_OBJECT_PRIVATE__
 
-#if HAVE_SYS_CDEFS_H
-#include <sys/cdefs.h>
-#endif
-#include <stddef.h>
 #include <os/object.h>
-
-#ifndef API_AVAILABLE
-#define API_AVAILABLE(...)
-#endif
+#include <stddef.h>
+#include <stdint.h>
 
 #if __GNUC__
 #define OS_OBJECT_NOTHROW __attribute__((__nothrow__))

commit d324892c766875a3e2e7f1f821f5ac6d18bd4ff1

Fix unistd.h check.
This patch fixes this compiler error.

/usr/include/dispatch/dispatch.h:38:45: error: expected value in expression
                                            ^

Fixes: 8af68ef3e4bb ("fix invalid use of config defines in API/SPI headers")
Signed-off-by: Vinson Lee <vlee@freedesktop.org>

dispatch/dispatch.h
@@ -35,7 +35,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdarg.h>
-#if !defined(HAVE_UNISTD_H) || HAVE_UNISTD_H
+#if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
 #include <fcntl.h>