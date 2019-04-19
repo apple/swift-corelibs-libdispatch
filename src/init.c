4d2bcef79f1453252ec28372998b42dd99ac3fce
Saleem Abdulrasool <compnerd@compnerd.org>
2019-04-19 15:43:11 -0700

    dispatch: fix a couple of printf specifiers
    Convert a couple of specifiers to PRI* style to make them portable.

--- src/init.c
+++ src/init.c
@@ -1045,7 +1045,7 @@ _dispatch_bug_kevent_vanished(dispatch_unote_t du)
 	_dispatch_log_fault("LIBDISPATCH_STRICT: _dispatch_bug_kevent_vanished",
 			"BUG in libdispatch client: %s, monitored resource vanished before "
 			"the source cancel handler was invoked "
-			"{ %p[%s], ident: %d / 0x%x, handler: %p }",
+			"{ %p[%s], ident: %" PRIdPTR " / 0x%" PRIxPTR ", handler: %p }",
 			dux_type(du._du)->dst_kind, dou._dq,
 			dou._dq->dq_label ? dou._dq->dq_label : "<unknown>",
 			du._du->du_ident, du._du->du_ident, func);
