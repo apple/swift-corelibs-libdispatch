1b4c03fb98c3f02c5d9ba2e53d944d38c8911f50
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-12 17:07:40 -0700

    Revert "Revert "Remove libbsd dependency for Linux""
    This reverts commit 4b87733f3e5e62a1c7a243d7e06cd35ae4b665b3.
    
    Signed-off-by: Kim Topley <ktopley@apple.com>

--- src/internal.h
+++ src/internal.h
@@ -282,8 +282,10 @@ upcast(dispatch_object_t dou)
 #include <linux/sysctl.h>
 #else
 #include <sys/sysctl.h>
-#include <sys/queue.h>
 #endif /* __ANDROID__ */
+#if !defined(__linux__)
+#include <sys/queue.h>
+#endif
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/mman.h>
