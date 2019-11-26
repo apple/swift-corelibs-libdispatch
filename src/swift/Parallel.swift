4e0d288dd39ba9be6cface200127812d4674479a
John Holdsworth <github@johnholdsworth.com>
2019-11-26 14:14:17 +0100

    Synchronizing without having to declare the locks separately.

--- src/swift/Parallel.swift
+++ src/swift/Parallel.swift
@@ -15,7 +15,7 @@ import Dispatch
 /// Wrapper for os_unfair_lock mutex primitve from the
 /// project: https://github.com/Alamofire/Alamofire
 @available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
-final public class UnfairLock {
+public final class UnfairLock {
     private let unfairLock: os_unfair_lock_t
 
     public init() {
@@ -41,9 +41,53 @@ final public class UnfairLock {
     /// - Parameter closure: The closure to run.
     ///
     /// - Returns:           The value the closure generated.
-    public func synchronized<T>(_ closure: () -> T) -> T {
+    func synchronized<T>(_ closure: () -> T) -> T {
         lock(); defer { unlock() }
         return closure()
+    }
+
+    /// Execute a closure while acquiring the lock.
+    ///
+    /// - Parameter closure: The closure to run.
+    func synchronized(_ closure: () -> Void) {
+        lock(); defer { unlock() }
+        return closure()
+    }
+}
+
+public typealias SynchronizableLock = UnfairLock
+private var allLocks = [OpaquePointer: SynchronizableLock]()
+private var lockLock = SynchronizableLock()
+
+protocol Synchronizable {
+    mutating func synchronized<T>(_ closure: (inout Self) -> T) -> T
+}
+
+extension Dictionary: Synchronizable {}
+extension Array: Synchronizable {}
+extension Int: Synchronizable {}
+
+extension Synchronizable {
+
+    public mutating func synchronized<T>(_ closure: (inout Self) -> T) -> T {
+        let lockee = UnsafeMutablePointer(&self)
+        let lock = lockLock.synchronized { () -> SynchronizableLock in
+            let key = OpaquePointer(lockee)
+            var lock = allLocks[key]
+            if lock == nil {
+                lock = SynchronizableLock()
+                allLocks[key] = lock
+            }
+            return lock!
+        }
+        return lock.synchronized { closure(&lockee.pointee) }
+    }
+
+    public mutating func desynchronize() {
+        let lockee = UnsafeMutablePointer(&self)
+        lockLock.synchronized {
+            allLocks[OpaquePointer(lockee)] = nil
+        }
     }
 }
 
@@ -88,20 +132,27 @@ extension Sequence {
     @available(OSX 10.12, iOS 13, tvOS 13, watchOS 6, *)
     @discardableResult
     func concurrentMap<Output>(maxConcurrency: Int = 4,
+                               initializer: Output? = nil,
                                queueName: String = "concurrentMap",
                                priority: DispatchQoS = .default,
                                worker: @escaping (Element,
                             @escaping (Output) -> Void) -> Void) -> [Output] {
         let input = Array(self)
-        var output = Array<Output>(unsafeUninitializedCapacity: input.count,
-                                   initializingWith: {
-                                    (buffer, initializedCount) in
-                                    let stride = MemoryLayout<Output>.stride
-                                    memset(buffer.baseAddress, 0,
-                                           stride * input.count)
-                                    initializedCount = input.count})
+        var output: [Output]
+        if initializer != nil {
+            output = Array(repeating: initializer!, count: input.count)
+        } else {
+            output = Array(unsafeUninitializedCapacity: input.count,
+                          initializingWith: {
+                            (buffer, initializedCount) in
+                            let stride = MemoryLayout<Output>.stride
+                            memset(buffer.baseAddress, 0,
+                                   stride * input.count)
+                            initializedCount = input.count})
+        }
 
-        output.withUnsafeMutableBufferPointer { buffer in
+        output.withUnsafeMutableBufferPointer {
+            buffer in
             let buffro = buffer
             let concurrentQueue = DispatchQueue(label: queueName, qos: priority,
                                                 attributes: .concurrent)
