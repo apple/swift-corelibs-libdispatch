f7396139cddf8fdfa869a161d65ee7c51b587775
John Holdsworth <github@johnholdsworth.com>
2019-11-28 13:08:28 +0100

    New "Cached" primitive

--- src/swift/Parallel.swift
+++ src/swift/Parallel.swift
@@ -14,6 +14,7 @@ import Dispatch
 
 /// Wrapper for os_unfair_lock mutex primitve from the
 /// project: https://github.com/Alamofire/Alamofire
+// Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
 @available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 public class UnfairLock {
     private let unfairLock: os_unfair_lock_t
@@ -55,6 +56,8 @@ public class UnfairLock {
     }
 }
 
+/// A wrapper for data to that is mutable across threads.
+@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 public class Synchronized<Wrapped>: UnfairLock {
     private var data: Wrapped
 
@@ -62,8 +65,41 @@ public class Synchronized<Wrapped>: UnfairLock {
         self.data = data
     }
 
+    /// Access to wrapped data after taking out a lock on it.
+    ///
+    /// - Parameter body: closure executed while wrapped data is locked
     public func synchronized<T>(_ body: (inout Wrapped) throws -> T) rethrows -> T {
         return try around { try body(&self.data) }
+    }
+}
+
+/// Simple synchronized cache
+@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+public class Cached<Key: Hashable, Value>: Synchronized<[Key: Value]> {
+
+    private let getter: (Key) -> Value
+
+    /// Initializer
+    ///
+    /// - Parameter getter: a closure used when key is not already cached.
+    public init(getter: @escaping (Key) -> Value) {
+        self.getter = getter
+        super.init([Key: Value]())
+    }
+
+    /// Get value for key, opulating if necessary
+    ///
+    /// - Parameter key: key of cache to fetch/generate value
+    public final func get(key: Key) -> Value {
+        return synchronized { data in
+            if let value = data[key] {
+                return value
+            }
+
+            let value = getter(key)
+            data[key] = value
+            return value
+        }
     }
 }
 
@@ -73,20 +109,21 @@ extension Sequence {
     /// and processes them on the number of threads specified.
     ///
     /// - Parameter maxConcurrency: Maximum number of threads to start.
-    /// - Parameter queueName: Name used for the concurrent queue
-    /// - Parameter priority: Quality of service of threads created
-    /// - Parameter worker: Closure to call to perform work
+    /// - Parameter initializer: a prototype to initialize output array.
+    /// - Parameter queueName: Name used for the concurrent queue.
+    /// - Parameter priority: Quality of service of threads created.
+    /// - Parameter worker: Closure to call to perform work.
     ///
     /// Up to maxConcurrency threads used to execute the `worker` closure
     /// passing in each value in the sequence and a closure to call when
     /// the work is complete and an output value is available.
     @available(OSX 10.12, iOS 13, tvOS 13, watchOS 6, *)
     @discardableResult
-    func concurrentMap<Output>(maxConcurrency: Int = 4,
-                               initializer: Output? = nil,
-                               queueName: String = "concurrentMap",
-                               priority: DispatchQoS = .default,
-                               worker: @escaping (Element,
+    public func concurrentMap<Output>(maxConcurrency: Int = 4,
+                                      initializer: Output? = nil,
+                                      queueName: String = "concurrentMap",
+                                      priority: DispatchQoS = .default,
+                                      worker: @escaping (Element,
                             @escaping (Output) -> Void) -> Void) -> [Output] {
         let input = Array(self)
         var output: [Output]
