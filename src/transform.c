4e1147fae6adf2183383232d66b53bae207e92f8
3405691582 <dsk@google.com>
2020-01-31 19:37:19 -0500

    Make some preliminary porting changes.
    These changes include:
    
      * hw_config.h: prevent referring to sysctlbyname on OpenBSD, as this
        is not available on all platforms.
    
      * transform.c: these stanzas referring to FreeBSD or Linux also apply
        to OpenBSD.
    
      * tests/dispatch_apply.c: stanza applying to Linux applies to OpenBSD
        and also tweak style for consistency.
    
      * tests/dispatch_io_net.c, tests/dispatch_test.h: stanzas applying to
        FreeBSD also apply to OpenBSD.
    
    Signed-off-by: Rokhini Prabhu <rokhinip@apple.com>

--- src/transform.c
+++ src/transform.c
@@ -26,7 +26,7 @@
 #include <endian.h>
 #define OSLittleEndian __LITTLE_ENDIAN
 #define OSBigEndian __BIG_ENDIAN
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
 #include <sys/endian.h>
 #define OSLittleEndian _LITTLE_ENDIAN
 #define OSBigEndian _BIG_ENDIAN
@@ -35,7 +35,7 @@
 #define OSBigEndian 4321
 #endif
 
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__)
 #define OSSwapLittleToHostInt16 le16toh
 #define OSSwapBigToHostInt16 be16toh
 #define OSSwapHostToLittleInt16 htole16
