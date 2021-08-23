16d9c1d17a85571e1ceb9cc721c6df331aaa5c91
Atari Dreams <83477269+AtariDreams@users.noreply.github.com>
2021-08-23 12:38:21 -0400

    Remove unused switch statement

--- src/init.c
+++ src/init.c
@@ -537,12 +537,6 @@ dispatch_queue_attr_t
 dispatch_queue_attr_make_with_autorelease_frequency(dispatch_queue_attr_t dqa,
 		dispatch_autorelease_frequency_t frequency)
 {
-	switch (frequency) {
-	case DISPATCH_AUTORELEASE_FREQUENCY_INHERIT:
-	case DISPATCH_AUTORELEASE_FREQUENCY_WORK_ITEM:
-	case DISPATCH_AUTORELEASE_FREQUENCY_NEVER:
-		break;
-	}
 	dispatch_queue_attr_info_t dqai = _dispatch_queue_attr_to_info(dqa);
 	dqai.dqai_autorelease_frequency = (uint16_t)frequency;
 	return _dispatch_queue_attr_from_info(dqai);
