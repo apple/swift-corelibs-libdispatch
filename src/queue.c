c4c351608431ab108b3d57081cd23720b2ac37f2
Saleem Abdulrasool <compnerd@compnerd.org>
2021-11-19 13:14:20 -0800

    dispatch: remove stray `void`
    The functions are meant to be aliases that are exported.  They should be
    defined to be the typeof the target function.  This corrects the
    aliasing.

--- src/queue.c
+++ src/queue.c
@@ -5765,11 +5765,11 @@ dispatch_channel_enqueue(dispatch_channel_t dch, void *ctxt)
 
 #ifndef __APPLE__
 #if __BLOCKS__
-void __typeof__(dispatch_channel_async) dispatch_channel_async
+__typeof__(dispatch_channel_async) dispatch_channel_async
 		__attribute__((__alias__("dispatch_async")));
 #endif
 
-void __typeof__(dispatch_channel_async_f) dispatch_channel_async_f
+__typeof__(dispatch_channel_async_f) dispatch_channel_async_f
 		__attribute__((__alias__("dispatch_async_f")));
 #endif
 
